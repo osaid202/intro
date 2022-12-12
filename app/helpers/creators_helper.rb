0cbb161fd5b08c8524530ee190ec35c9cf3244e4
osaid202 <osaid202@gmail.com>
2022-12-12 08:37:58 -0500

    Static maps

--- app/helpers/creators_helper.rb
+++ app/helpers/creators_helper.rb
@@ -1,2 +1,5 @@
 module CreatorsHelper
+  def google_map(center)
+    "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=400x400&zoom=14&key=AIzaSyAxLs9qW6C6vP0daTF47-ZUVdbAzde1lsQ"
+  end
 end
